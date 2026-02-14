ab3624fad0d1b29ac6ab0b63c157f740b2871799
Helena Oliveira <helena@informach.com.br>
2026-02-14 14:36:03 -0300

    add leva and drei

--- src/index.jsx
+++ src/index.jsx
@@ -2,24 +2,23 @@ import './style.css'
 import ReactDOM from 'react-dom/client'
 import { Canvas } from '@react-three/fiber'
 import Experience from './Experience.jsx'
-import * as THREE from 'three'
+import { StrictMode } from 'react'
+import { Leva } from 'leva'
 
 const root = ReactDOM.createRoot(document.querySelector('#root'))
 
 root.render(
-    <Canvas
-        gl={{
-            antialias: true,
-            toneMapping: THREE.ACESFilmicToneMapping,
-            // outputColorSpace: THREE.SRGBColorSpace
-        }}
-        camera={{
-            fov: 45,
-            near: 0.1,
-            far: 200,
-            position: [3, 2, 6]
-        }}
-    >
-        <Experience />
-    </Canvas>
+    <StrictMode>
+        <Leva collapsed />
+        <Canvas
+            camera={{
+                fov: 45,
+                near: 0.1,
+                far: 200,
+                position: [- 4, 3, 6]
+            }}
+        >
+            <Experience />
+        </Canvas>
+    </StrictMode>
 )
